1beb3d7065f5d34e10d260c9a10f8b38773d7283
Meghna Srivastava <meghna.srivastava@auto1.com>
2019-04-09 12:30:26 +0200

    added global constants

--- src/components/Keyboard/index.tsx
+++ src/components/Keyboard/index.tsx
@@ -1,12 +1,27 @@
 import * as React from 'react';
+import { DIGITS, OPERATORS } from '../../constants/globalConstants';
 
 export interface KeyboardProps {
 }
 
-export function Keyboard (props: KeyboardProps) {
-    return (
-      <div>
+export interface KeyboardState {
+}
+
+
+export default class Keyboard extends React.Component<KeyboardProps, KeyboardState> {
+    constructor(props: KeyboardProps) {
+        super(props);
+
+        this.state = {
+        }
+    }
+
+    public render() {
         
-      </div>
-    );
+        return (
+            <React.Fragment>
+               
+            </React.Fragment>
+        );
+    }
 }
---
+++ src/constants/globalConstants.tsx
@@ -0,0 +1,9 @@
+export const DIGITS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+export const OPERATORS = [
+    { operation: 'divide', symbol: '/' },
+    { operation: 'multiply', symbol: '*' },
+    { operation: 'add', symbol: '+' },
+    { operation: 'substract', symbol: '-' },
+    { operation: 'reset', symbol: 'AC' }
+]
