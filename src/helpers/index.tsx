8375d56a0bee8e56fe73b00a6c4a7aa1b3a17491
Meghna Srivastava <meghna.srivastava@auto1.com>
2019-04-10 14:00:50 +0200

    refactored getCalculationResult helper

--- src/helpers/index.tsx
+++ src/helpers/index.tsx
@@ -1,21 +1,34 @@
-export const getCalculationResult = (inputOne: number | null, operator: string, inputTwo: number | null) => {
+export const getCalculationResult = (inputOne: number | null, operator: string, inputTwo: number | null): number => {
+    let result = 0;
+
     switch (operator) {
         case '+':
             if (inputOne && inputTwo)
-            return Number(inputOne) + Number(inputTwo)
+                return result = Number(inputOne) + Number(inputTwo)
             break;
         case '-':
             if (inputOne && inputTwo)
-                return Number(inputOne) - Number(inputTwo)
+                return result = Number(inputOne) - Number(inputTwo)
             break;
         case '*':
             if (inputOne && inputTwo)
-                return Number(inputOne) * Number(inputTwo)
+                return result = Number(inputOne) * Number(inputTwo)
             break;
         case '/':
             if (inputOne && inputTwo)
-                return Number(inputOne) / Number(inputTwo)
+                return result = Number(inputOne) / Number(inputTwo)
             break;
+        case '%':
+            if (inputOne && inputTwo)
+                return result = Number(inputOne) % Number(inputTwo)
+            break;
+        case '^':
+            if (inputOne && inputTwo)
+                return result = Number(inputOne) ^ Number(inputTwo)
+            break;
+        default:
+            null
     }
 
+    return result;
 }
